f4e3f7a7537cad9e21bae00cf5eeb172166cc7bb
khairul111010 <khairul18103199@gmail.com>
2023-02-25 15:50:29 +0600

    dashboard

---
+++ src/pages/home.tsx
@@ -0,0 +1,44 @@
+import { Box, Stack, Typography } from '@pankod/refine-mui'
+import { PieChart, TotalRevenue, PropertyReferrals } from "components";
+import React from 'react'
+
+const home = () => {
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart title="Properties for Sale"
+        value={684}
+        series={[75, 25]}
+        colors={['#275be8','#c4e8ef']}
+        />
+        <PieChart title="Properties for Rent"
+        value={550}
+        series={[60, 40]}
+        colors={['#275be8','#c4e8ef']}
+        />
+        <PieChart title="Total Customers"
+        value={5684}
+        series={[75, 25]}
+        colors={['#275be8','#c4e8ef']}
+        />
+        <PieChart title="Total Cities"
+        value={555}
+        series={[75, 25]}
+        colors={['#275be8','#c4e8ef']}
+        />
+      </Box>
+
+
+      <Stack mt="25px" width="100%" direction={{xs: 'column', lg: 'row'}}>
+        <TotalRevenue/>
+        <PropertyReferrals/>
+      </Stack>
+    </Box>
+  )
+}
+
+export default home
---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import AgentProfile from "./agent-profile";
+import Agents from "./agent";
+import AllProperties from "./all-properties";
+import CreateProperty from "./create-property";
+import EditProperty from "./edit-property";
+import Home from "./home";
+import { Login } from "./login";
+import MyProfile from "./my-profile";
+import PropertyDetails from "./property-details";
+
+
+export {
+    AgentProfile,
+    Agents,
+    AllProperties,
+    CreateProperty,
+    EditProperty,
+    Home,
+    Login,
+    MyProfile,
+    PropertyDetails
+}
